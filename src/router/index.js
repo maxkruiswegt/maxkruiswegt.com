f1bfe5d5b659762daf569a50d66db4a3199d3bbe
Max Kruiswegt <112982786+ItsMaxDev@users.noreply.github.com>
2024-07-29 13:40:58 +0200

    Add improved scroll behavior

--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,98 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import { i18n } from '../main'
+import { createRouter, createWebHistory } from 'vue-router';
+import { i18n } from '../main';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return {
+        ...savedPosition,
+        behavior: 'smooth',
+      };
+    } else if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: 'smooth',
+      };
+    } else {
+      return { top: 0 };
+    }
+  },
   routes: [
-    { path: '/', name: 'home_en', component: () => import('../views/Home.vue') },
-    { path: '/portfolio', name: 'portfolio_en', component: () => import('../views/Portfolio.vue') },
-    { path: '/contact', name: 'contact_en', component: () => import('../views/Contact.vue') },
-    { path: '/portfolio/:id', name: 'project_en', component: () => import('../views/Project.vue'), props: true },
+    {
+      path: '/',
+      name: 'home_en',
+      component: () => import('../views/Home.vue'),
+    },
+    {
+      path: '/portfolio',
+      name: 'portfolio_en',
+      component: () => import('../views/Portfolio.vue'),
+    },
+    {
+      path: '/contact',
+      name: 'contact_en',
+      component: () => import('../views/Contact.vue'),
+    },
+    {
+      path: '/portfolio/:id',
+      name: 'project_en',
+      component: () => import('../views/Project.vue'),
+      props: true,
+    },
     {
       path: '/nl',
       children: [
-        { path: '', name: 'home_nl', component: () => import('../views/Home.vue') },
-        { path: 'portfolio', name: 'portfolio_nl', component: () => import('../views/Portfolio.vue') },
-        { path: 'contact', name: 'contact_nl', component: () => import('../views/Contact.vue') },
-        { path: 'portfolio/:id', name: 'project_nl', component: () => import('../views/Project.vue'), props: true }
-      ]
+        {
+          path: '',
+          name: 'home_nl',
+          component: () => import('../views/Home.vue'),
+        },
+        {
+          path: 'portfolio',
+          name: 'portfolio_nl',
+          component: () => import('../views/Portfolio.vue'),
+        },
+        {
+          path: 'contact',
+          name: 'contact_nl',
+          component: () => import('../views/Contact.vue'),
+        },
+        {
+          path: 'portfolio/:id',
+          name: 'project_nl',
+          component: () => import('../views/Project.vue'),
+          props: true,
+        },
+      ],
     },
     {
       path: '/admin',
       children: [
-        { path: '', name: 'admin', component: () => import('../views/admin/AdminHome.vue') },
-        { path: 'users', name: 'admin_users', component: () => import('../views/admin/AdminUsers.vue') }
-      ]
+        {
+          path: '',
+          name: 'admin',
+          component: () => import('../views/admin/AdminHome.vue'),
+        },
+        {
+          path: 'users',
+          name: 'admin_users',
+          component: () => import('../views/admin/AdminUsers.vue'),
+        },
+      ],
     },
-    { path: '/:pathMatch(.*)*', name: '404', component: () => import('../views/404.vue') },
-  ]
-})
+    {
+      path: '/:pathMatch(.*)*',
+      name: '404',
+      component: () => import('../views/404.vue'),
+    },
+  ],
+});
 
 router.beforeEach((to, _, next) => {
-  const language = to.fullPath.startsWith('/nl') ? 'nl' : 'en'
-  i18n.global.locale.value = language
-  next()
-})
+  const language = to.fullPath.startsWith('/nl') ? 'nl' : 'en';
+  i18n.global.locale.value = language;
+  next();
+});
 
-export default router
+export default router;
