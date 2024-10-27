6876074c3baf6e86528e64de09621954d4250a06
Max Kruiswegt <112982786+ItsMaxDev@users.noreply.github.com>
2024-10-27 12:02:15 +0100

    Update home image, add cool glasses on hover and confetti on click

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { onMounted } from 'vue';
 import { useI18n } from 'vue-i18n';
+import confetti from 'canvas-confetti';
 
 const { t, locale } = useI18n();
 
@@ -24,22 +25,51 @@ onMounted(() => {
     }
   }
 });
+
+function switchImage(event, newSrc) {
+  event.target.src = newSrc;
+}
+
+function triggerConfetti() {
+  confetti({
+    particleCount: 100,
+    spread: 70,
+    origin: { y: 0.6 },
+  });
+}
 </script>
 
 <template>
   <div class="mt-[10vh] mb-[20vh] lg:mt-[25vh] lg:mb-[35vh]">
     <div class="flex flex-col items-center lg:flex-row gap-8 lg:gap-20 main">
       <div class="hidden-element">
-        <img src="/images/Max.webp" alt="Max Kruiswegt" class="h-60 lg:h-80" />
+        <img
+          src="/images/Max.webp"
+          alt="Max Kruiswegt"
+          class="h-72 lg:h-96 cursor-pointer"
+          @mouseover="switchImage($event, '/images/Max_Cool.webp')"
+          @mouseout="switchImage($event, '/images/Max.webp')"
+          @click="triggerConfetti"
+        />
       </div>
       <div class="hidden-element">
         <div class="text-center lg:text-left">
           <h1 v-html="t('home.title')"></h1>
         </div>
-        <div class="flex flex-col sm:flex-row justify-center lg:justify-normal gap-4 mt-4">
-          <RouterLink :to="{ name: `portfolio_${locale}` }" class="btn btn-primary" v-html="t('home.buttonWork')">
+        <div
+          class="flex flex-col sm:flex-row justify-center lg:justify-normal gap-4 mt-4"
+        >
+          <RouterLink
+            :to="{ name: `portfolio_${locale}` }"
+            class="btn btn-primary"
+            v-html="t('home.buttonWork')"
+          >
           </RouterLink>
-          <RouterLink :to="{ name: `contact_${locale}` }" class="btn btn-secondary" v-html="t('home.buttonContact')">
+          <RouterLink
+            :to="{ name: `contact_${locale}` }"
+            class="btn btn-secondary"
+            v-html="t('home.buttonContact')"
+          >
           </RouterLink>
         </div>
       </div>
