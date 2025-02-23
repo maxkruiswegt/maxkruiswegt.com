fdcc9921cb3d9347e2bd1d5b7ded6ede9a04d7ea
Max Kruiswegt <112982786+maxkruiswegt@users.noreply.github.com>
2025-02-23 14:21:44 +0100

    Refactor Portfolio.vue for improved mobile-first layout and styling

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -6,7 +6,7 @@ const { t, tm } = useI18n();
 </script>
 
 <template>
-  <div>
+  <div class="portfolio">
     <h1 class="hidden-element">{{ t('portfolio.title') }}</h1>
     <p class="hidden-element">{{ t('portfolio.description') }}</p>
     <div class="portfolio-projects">
@@ -25,41 +25,50 @@ const { t, tm } = useI18n();
 </template>
 
 <style scoped>
-/* Extra Small Devices (Less than 576px) */
+/* Default styles: Mobile first */
+.portfolio {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+
+  h1,
+  p {
+    text-align: center;
+  }
+}
+
 .portfolio-projects {
   display: grid;
   grid-template-columns: repeat(1, minmax(0, 1fr));
   gap: 1rem;
   margin-top: 2rem;
+  width: 100%;
 }
 
-/* Small Devices */
-@media screen and (min-width: 576px) {
+/* Smaller phone viewpoints (380px and up) */
+@media screen and (min-width: 380px) {
 }
 
-/* Medium Devices */
-@media screen and (min-width: 768px) {
+/* Small devices and most phones (480px and up) */
+@media only screen and (min-width: 480px) {
+}
+
+/* Most tablets (768px and up) */
+@media only screen and (min-width: 768px) {
   .portfolio-projects {
     grid-template-columns: repeat(2, minmax(0, 1fr));
   }
 }
 
-/* Large Devices*/
-@media screen and (min-width: 992px) {
+/* Smaller desktop viewpoints (992px and up) */
+@media only screen and (min-width: 992px) {
+}
+
+/* Large devices and wide screens (1200px and up) */
+@media only screen and (min-width: 1200px) {
   .portfolio-projects {
     grid-template-columns: repeat(3, minmax(0, 1fr));
   }
-}
-
-/* Extra Large Devices */
-@media screen and (min-width: 1200px) {
-}
-
-/* Extra Extra Large Devices */
-@media screen and (min-width: 1600px) {
-}
-
-/* Very Large Devices */
-@media screen and (min-width: 2560px) {
 }
 </style>
