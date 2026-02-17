5832fe2ed8ed9840b8ed8f6b13fcbea585c549f2
Max Kruiswegt <112982786+maxkruiswegt@users.noreply.github.com>
2026-02-17 17:13:58 +0100

    Add background color to project card for improved visibility

--- app/components/ProjectPreview.vue
+++ app/components/ProjectPreview.vue
@@ -34,6 +34,7 @@ defineProps<{
   flex-direction: column;
   border-radius: 0.75rem;
   overflow: hidden;
+  background-color: var(--background);
   border: 1px solid var(--background-20);
   text-decoration: none;
   color: inherit;
